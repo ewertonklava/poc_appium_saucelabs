1c1d9aaff936b403868383cb03d3c9fd706c96dd
Luiz Pereira <lcnpj@outlook.com>
2018-11-08 23:42:46 -0200

    aeee krl

--- features/screens/base_screen.rb
+++ features/screens/base_screen.rb
@@ -1,30 +1,11 @@
 class BaseScreen
 
   def fill_text_field(element, text, time_out=60)
-    driver.find_element(element).click
-    driver.find_element(element).send_keys(text)
-    hide_keyboard_action
+    @driver.find_element(:xpath, element).click
+    @driver.find_element(:xpath, element).send_keys(text)
+    @driver.hide_keyboard
   rescue => e
     raise "Erro ao preencher campo #{element}.\nErro: #{e.message}"
-  end
-
-#Método para esperar um elemento mapeado estar visível na tela por xpath durante 10s. 
-  def wait_for_xpath(xpath, timeout = 60)
-    @driver.wait_true(timeout) do
-    @driver.exists { @driver.find_element(:xpath, xpath) }
-    end
-  end
-
-#Método para esperar um elemento mapeado estar visível na tela por id durante 10s.
-  def wait_for_id(id, timeout = 60)
-    @driver.wait_true(timeout) do 
-      @driver.exists { @driver.find_element(:id, id) }
-    end
-  end
-
-#Método para clicar um elemento mapeado na tela por id.
-  def click_id(id)
-    @driver.find_element(:id, id).click
   end
 
 #Método para clicar um elemento mapeado na tela por xpath.
@@ -32,13 +13,4 @@ class BaseScreen
     @driver.find_element(:xpath, xpath).click
   end
 
-#Método para preencher um campo mapeado na tela por id.
-  def send_keys_id(id, field)
-    @driver.find_element(:id, id).send_keys(field)
-  end
-
-#Método para preencher um campo mapeado na tela por xpath.
-  def send_keys_xpath(xpath, field)
-    @driver.find_element(:xpath, xpath).send_keys(field)
-  end
 end
--- features/step_definitions/cadastrar.rb
+++ features/step_definitions/cadastrar.rb
@@ -6,19 +6,19 @@ Dado(/^preencher os capos de cadastro$/) do
   @cadastro = CadastroScreen.new
   @actions = BaseScreen.new
   
-  @actions.wait_for_xpath(@cadastro.campo_nome)
-  @actions.send_keys_xpath(@cadastro.campo_nome, @nome_cadastro)
+  #@actions.wait_for_xpath(@cadastro.campo_nome)
+  @actions.fill_text_field(@cadastro.campo_nome, @nome_cadastro)
   
-  @actions.wait_for_xpath(@cadastro.campo_email)
-  @actions.send_keys_xpath(@cadastro.campo_email, @email)
+  #@actions.wait_for_xpath(@cadastro.campo_email)
+  @actions.fill_text_field(@cadastro.campo_email, @email)
 
-  @actions.wait_for_xpath(@cadastro.check_tipo)
+  #@actions.wait_for_xpath(@cadastro.check_tipo)
   @actions.click_xpath(@cadastro.check_tipo)
 end
 
 Quando(/^efetuar o cadastro$/) do
-  hide_keyboard
-  @actions.wait_for_xpath(@cadastro.botao_enviar)
+  scroll_to_exact("ENVIAR")
+  #@actions.wait_for_xpath(@cadastro.botao_enviar)
   @actions.click_xpath(@cadastro.botao_enviar)
 end
 
@@ -26,12 +26,4 @@ Então(/^validar os dados do usuario cadastrado$/) do
   @driver.wait_true(5) do
     @driver.exists { driver.find_element(id:'alertTitle') }
   end
-end
-
-Então(/^subir device$/) do
-  binding.pry
-  driver.find_element(id:'cling_dismiss_longpress_info').click
-  driver.find_element(xpath:'//android.widget.TextView[@content-desc="Apps"]').click
-  scroll_to_exact("tdc_mobile_app")
-  driver.find_element(id:'tdc_mobile_app').click
 end
